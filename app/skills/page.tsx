5a1702af00269ec1d4f1d369047003358d8bd9d5
SantoshChapagai <santoshchapagai100@gmail.com>
2023-12-12 20:08:12 +0200

    practice the routing

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div className='min-h-screen'>Skills</div>
+    <div className='min-h-screen'>
+      <h2>Front End</h2>
+      <h2>Backend</h2>
+      <h2>Databases</h2>
+    </div>
   )
 }
 
