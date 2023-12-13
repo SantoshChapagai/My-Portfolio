654922544fc3e7f097c04cd71ecc989da0a9eab0
SantoshChapagai <santoshchapagai100@gmail.com>
2023-12-13 21:59:04 +0200

    add content to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+    <main className="flex flex-cols items-center justify-between p-32">
+      <div className='max-w-[600px]'>
+      <h1>Hello! I'm Santosh Chapagai</h1>
+      <h3>A Full-Stack Developer</h3>
+      <p>I am a Master's in Business Administration graduate and a full stack developer. I am going to complete my full-stack program from Business College Helsinki. Currently, I am looking for an internship in the field of web development.</p>
+      </div>
+      <div>
+        <Image src="/santosh.jpg" alt="santosh" width={100} height={80} className="object-contain"/>
+      </div>
     </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
-import About from '../pages/about';
+
 
 const Navbar = () => {
   return (
