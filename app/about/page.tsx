ef739e805c184caed79ed1d468f9bf18d874fb74
SantoshChapagai <santoshchapagai100@gmail.com>
2023-12-26 20:40:57 +0200

    add content to about and skills section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,20 @@ import React from 'react'
 const About = () => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>I am a Master's in Business Administration gradudate and a full stack developer. </p>
+      <div className='max-w-[750px]'>
+        <p>
+        I am a graduate of a Master's of Business Administration, specializing in International Business. Now, I'm learning all about making websites at Business College Helsinki. For about a year, I've been learning about cool things like React and Drupal in my classes. I also took a course on making whole websites using React and Node JS on Udemy. When I'm not studying, I love exploring nature. I also play sports, spend time gaming on my PS4, and hang out with my family and friends. I'm excited about mixing my business skills with what I'm learning in tech. The world of making websites keeps changing, and I find it super interesting. I'm always curious about new things in web development.
+        </p>
+      </div>
+      <div>
+        <h1>Journey to be a developer</h1>
+      </div>
+      <div>
+        <h1>Educational Background</h1>
+      </div>
+      <div>
+        <h1>Hobbies</h1>
+      </div>
     </div>
   )
 }
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen p-24'>
+      <div>
       <h2>Front End</h2>
+      </div>
+      <div>
       <h2>Backend</h2>
+      </div>
+      <div>
       <h2>Databases</h2>
+      </div>
+      <div>
+        <h2> Others </h2>
+      </div>
+      
     </div>
   )
 }
