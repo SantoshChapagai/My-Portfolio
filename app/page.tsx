b84e0a5a03535901502d174699205beb37cae029
SantoshChapagai <santoshchapagai100@gmail.com>
2023-12-27 17:25:54 +0200

    add content to the portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <main className="flex flex-cols items-center justify-evenly p-24">
       <div className='lg:max-w-[750px] mr-4'>
       <h1 className='text-3xl text-bold p-4'>Hello! I'm Santosh Chapagai</h1>
-      <h3 className='text-xl p-4'>A Full-Stack Developer <span className='transition shake'>👋</span></h3>
+      <h3 className='text-xl p-4'>A Full-Stack Developer <span className='transition duration-1000 ease-in animate-pulse'>👋</span></h3>
       <p className='p-4'>I am a Master's in Business Administration graduate and a full stack developer. I am going to complete my full-stack program from Business College Helsinki. Currently, I am looking for an internship in the field of web development.</p>
       </div>
       <div>
-        <Image src="/santosh.jpg" alt="santosh" width={300} height={380} className="object-contain rounded-full h-72 w-72 pt-8"/>
+        <Image src="/santosh.jpg" alt="santosh" width={768} height={512} className="object-contain rounded-full h-96 w-84 pt-8"/>
       </div>
     </main>
   )
