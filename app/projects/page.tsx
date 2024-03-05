262380f1bc17ef3c6b18fa48823ba30998a95c94
SantoshChapagai <santoshchapagai100@gmail.com>
2024-03-05 20:36:55 +0200

    add content to the project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -21,14 +21,14 @@ const Projects = () => {
       <div className='max-w-[700px] mb-8'>
         <p>I have developed some cool projects as a part of my study during the time at Business College Helsinki. When I started learning coding I developed focused on the front-end and as the study progress and get to know about backend I have developed some full stack application. I have also developed some site using publicly availiable APIs. Moreover, I have also developed some project as a part of my study in Udemy.</p>
       </div>
-      <div>
-        <div>
-          <div className='flex flex-col lg:flex-row justify-between items-center'>
+      <div className='flex flex-col items-center'>
+        <div className='lg:max-w-[1000px]'>
+          <div className='flex flex-col lg:flex-row justify-around items-center mt-5'>
           <div className='space-y-4'>
           <div className='text-center'>
           <TypographyH3>Recipe App</TypographyH3>
           </div>
-          <p className='max-w-[700px] mx-8'>Built with React and CSS, this recipe app displays basic recipe information on cards, allowing users to navigate to individual recipes for more details. Users can also add their favorite recipes to a list.</p>
+          <p className='max-w-[500px] mx-8'>Built with React and CSS, this recipe app displays basic recipe information on cards, allowing users to navigate to individual recipes for more details. Users can also add their favorite recipes to a list.</p>
           <Link href="https://github.com/SantoshChapagai/recipe-react" target='blank' className='flex justify-center hover:underline'>See more at: <FaGithub size={28}/></Link>
           </div>
           <div className='w-[500px]'>
@@ -36,13 +36,13 @@ const Projects = () => {
           </div>
           </div>
         </div>
-        <div>
-          <div className='flex flex-col lg:flex-row-reverse justify-between items-center'>
+        <div className='lg:max-w-[1000px]'>
+          <div className='flex flex-col lg:flex-row-reverse justify-around items-center mt-5'>
           <div className='space-y-4'>
           <div className='text-center'>
           <TypographyH3>Shopping App</TypographyH3>
           </div>
-          <p className='max-w-[700px] mx-8'>Built with React and Bootstrap, this website integrates the Fakestore API to showcase product details via cards with images. Users can filter products by category, gender, and prices.</p>
+          <p className='max-w-[500px] mx-8'>Built with React and Bootstrap, this website integrates the Fakestore API to showcase product details via cards with images. Users can filter products by category, gender, and prices.</p>
           <Link href="https://github.com/SantoshChapagai/shopping-app" target='blank' className='flex justify-center hover:underline'>See more at: <FaGithub size={28}/></Link>
           </div>
           <div className='w-[500px]'>
@@ -50,17 +50,32 @@ const Projects = () => {
           </div>
           </div>
         </div>
-        <div>
-          <div className='flex flex-col lg:flex-row justify-between items-center'>
+        <div className='lg:max-w-[1000px]'>
+          <div className='flex flex-col lg:flex-row justify-around items-center lg:mt-5'>
           <div className='space-y-4'>
           <div className='text-center'>
           <TypographyH3>Speed Game</TypographyH3>
           </div>
-          <p className='max-w-[700px] mx-8'>Built with React, this game features three levels: Easy, with 4 circles; Medium, with 5 circles; and Hard, with 6 circles. The cat appears randomly in one of the circles. If the player clicks the circle where the cat appears, 10 points are added to the score.</p>
+          <p className='max-w-[500px] mx-8'>Built with React, this game features three levels: Easy, with 4 circles; Medium, with 5 circles; and Hard, with 6 circles. The cat appears randomly in one of the circles. If the player clicks the circle where the cat appears, 10 points are added to the score.</p>
           <Link href="https://github.com/SantoshChapagai/speedgame-react" target='blank' className='flex justify-center hover:underline'>See more at: <FaGithub size={28}/></Link>
           </div>
           <div className='w-[500px]'>
             <Video ref={videoRef} src="videos/speedgame.mp4" autoPlay muted loop/>
+          </div>
+          </div>
+        </div>
+        <div className='lg:max-w-[1000px] mt-4'>
+          <div className='flex flex-col lg:flex-row-reverse justify-around items-center lg:mt-5'>
+          <div className='space-y-4'>
+          <div className='text-center'>
+          <TypographyH3>Blog</TypographyH3>
+          </div>
+          <p className='w-[300px] lg:w-[500px] mx-8'>
+Built with Node, Express, and EJS, this web application uses MongoDB for the database. Users can compose blogs, and all composed blogs are listed on the home page.</p>
+          <Link href="https://github.com/SantoshChapagai/blog-mongodb" target='blank' className='flex justify-center hover:underline p-2'>See more at: <FaGithub size={28}/></Link>
+          </div>
+          <div className='w-[320px] lg:w-[500px]'>
+            <Video ref={videoRef} src="videos/blog.mp4" autoPlay muted loop/>
           </div>
           </div>
         </div>
@@ -70,12 +85,6 @@ const Projects = () => {
         </div>
         <div className='rounded-3xl w-96 overflow-hidden'>
           <video src="./videos/recip.mp4" autoPlay autoFocus></video>
-        </div>
-        <div>
-          <TypographyH3>ToDo</TypographyH3>
-          <p>Built with Node, EJS, MongoDb, and CSS. It is the simple app where the current date is displayed on the top and user can add the todo ietm on the list. User can also mark the completed task clicking the checkbox.</p>
-          <div className='rounded-3xl'>
-          </div>
         </div>
         <div className='flex justify-center items-center py-4'>
           <TypographyH1>See more of my work in</TypographyH1>
